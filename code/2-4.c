711ba3089ee55a69b1721f8709fb009d92d8f62b
qkrtlgus0225-coder <qkrtlgus0225@gmail.com>
2026-04-08 13:47:22 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -4,8 +4,6 @@ long my_strtol(char *str, char **endptr) {
     long res = 0;
 
     while (*str >= '0' && *str <= '9') {
-       (res * 10)
-       (*str - '0')
         res = res * 10 + (*str - '0');
         str++; 
     }
